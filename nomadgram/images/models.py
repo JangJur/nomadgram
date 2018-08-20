e128eac75e81c62b7f6b31604bc7d5d05236dc6b
JangJur <wkdwndud753@naver.com>
2018-08-20 00:20:39 -0700

    Created the User, Image, Comment, Timestamped Model and Foreing Keys

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from nomadgram.users import models as user_models
 
 
 class TimeStampModel(models.Model):
@@ -10,15 +11,27 @@ class TimeStampModel(models.Model):
         abstract = True
 
 
-
-
 class Image(TimeStampModel):
+
+    """ Image Model """
 
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True)
 
 
 class Comment(TimeStampModel):
 
+    """ Comment Model """
+
     message = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
+
+class Like(TimeStampModel):
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
